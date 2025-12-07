5a416f07a34c077b68b906a944df0f23720b4ced
A Jatin Ram Chowdary <jatinramchowdary1980@gmail.com>
2025-12-07 11:17:16 +0530

    Initial commit

---
+++ smarteat-backend/src/main/java/com/example/smarteat_backend/SmarteatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarteat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarteatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarteatBackendApplication.class, args);
+	}
+
+}
---
+++ smarteat-backend/src/test/java/com/example/smarteat_backend/SmarteatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarteat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarteatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
